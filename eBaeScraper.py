0ce8020633fd65c6fe5fa8dd5d0a6703d92592b8
IsolatedPotato <89231270+ChumboGT@users.noreply.github.com>
2022-07-28 10:59:17 -0700

    Sucessfully scrapes 60 items from the url into csv, todos to come

--- eBaeScraper.py
+++ eBaeScraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 item_names = []
 item_prices = []
+new_item_bool = []
 
 ebay_store_url = 'https://www.ebay.com/sch/1920-29/37832/m.html?_ssn=the-ad-store'
 response = requests.get(ebay_store_url)
@@ -15,20 +16,22 @@ listings = soup.find_all('li', attrs={'class': 'sresult'})
 for listing in listings:
     prod_name=" "
     prod_price = " "
+    new_prod = 0
     
     h3_lvtitle = listing.find('h3', attrs={'class':"lvtitle"})
 
     if (h3_lvtitle.a.title!="Nothing"):
         title = str(h3_lvtitle.a.contents[0]).replace("\r","").replace("\n","").replace("\t","")
 
-        if (prod_name==" "):
-            prod_name=f"{title}"
-            item_names.append(title);
-        #else:
-            #prod_name=f"{prod_name},{title}"
+        if ("New listing" in title):
+            title = str(h3_lvtitle.a.contents[1]).replace("\r","").replace("\n","").replace("\t","")
+            new_prod = 1
+        
+        item_names.append(title);
+        new_item_bool.append(new_prod);
 
     #If products are found, find the prices
-    if(prod_name!=" "):
+    #if(prod_name!=" "):
         li_lvprice = listing.find('li', attrs={'class':"lvprice"})
         price = str(li_lvprice.find('span', attrs={'class':"bold"}).contents[0]).removeprefix("\n\t\t\t\t\t")
 
@@ -43,7 +46,7 @@ from scipy import stats
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({"Name":item_names, "Prices": item_prices})
+df = pd.DataFrame({"New Listing":new_item_bool,"Name":item_names, "Prices": item_prices})
 df.to_csv('C:\_git\python\eBayScraperthe-ad-store-listings.csv')
 #data_note_8 = data_note_8.ilociloc[np.abs(stats.zscore(data_note_8["Prices"])) lt; 3,]
 #print(response)
